56be11b6c23380e17ebe3c087ba28ca726e2ca6f
JSV <jacqueline@tr7telecombrasil.com>
2020-10-31 00:38:26 -0300

    Refactoring in PHP

--- webpages/products.php
+++ webpages/products.php
@@ -37,165 +37,175 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="../styles/main.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <title>Produtos - Full Stack Eletro</title>
     </head>
 
     <body>
-
-        <!--Menu-->
+        <!----------MENU SECTION------------>
         <?php
             include('menu.html');
         ?>
 
-
-        <header class="header">
-            <h1>Produtos</h1>
-        </header>
-        <hr>
-
-
-        <!--Categorias-->
-        <section class="categories">
-            <h3>Categorias</h3>
-            <ul>
-                <li onclick="show_all()">Todos (12)</li>
-                <li onclick="show_category('geladeira')">Geladeiras (3)</li>
-                <li onclick="show_category('fogao')">Fogões (2)</li>
-                <li onclick="show_category('microondas')">Microondas (3)</li>
-                <li onclick="show_category('lavaroupa')">Lavadora de roupas (2)</li>
-                <li onclick="show_category('lavalouca')">Lava-louças (2)<li></li>                      
-            </ul>
-        </section>
-
         
-        <!--Produtos Cadastrados-->
-        <section class="products">
+        <div class="container">
 
-            <?php
-            // Consultando e reinderizando os dados já armazenados no banco de dados
-            $sql_produtos = "SELECT * FROM produtos";
-            $result = $conn->query($sql_produtos);
+            <!---------CATEGORIES SECTION-------->
+            <section class="categories center">
+                <h5 class="tittle">Categorias</h5>
+                <div class="space">
+                    <button class="btn btn-outline-info" onclick="show_all()">Todos</button>
+                    <button class="btn btn-outline-info" onclick="show_category('microondas')">Microondas</button>
+                    <button class="btn btn-outline-info" onclick="show_category('geladeira')">Geladeiras</button>
+                    <button class="btn btn-outline-info" onclick="show_category('lavaroupa')">Lava-roupas</button>
+                    <button class="btn btn-outline-info" onclick="show_category('fogão')">Fogões</button>
+                    <button class="btn btn-outline-info" onclick="show_category('lavalouca')">Lava-louças</button>
+                </div>
+            </section>
 
-            if($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {                    
-            ?>
 
-                    <div class="box-product" id="<?php echo $row['categoria'];?>" style="display: block;">
+            <!---------PRODUCTS SECTION-------->
+            <section class="products center">
+
+                <?php
+                // Consultando e reinderizando os dados já armazenados no banco de dados
+                $sql_produtos = "SELECT * FROM produtos";
+                $result = $conn->query($sql_produtos);
+
+                if($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {                    
+                ?>
+                
+                <div class="box-product" id="<?php echo $row['categoria'];?>">
                     <img onmouseover="mouseOver(this)" onmouseout="mouseOut(this)" src="<?php echo $row['imagem'];?>">
                     <br>
                     <p class="description"><?php echo $row['descricao'];?></p>
                     <hr>
                     <p class="previousvalue"><?php echo $row['preco_anterior'];?></p>
                     <p class="currentvalue"><?php echo $row['preco_final'];?></p>
-                </div>
+                </div>         
 
-            <?php
-                }
-            } else {
-                echo "Nenhum produto cadastrado. Tente outra vez.";
-            }        
-            ?>
-        </section>
-
-        <div class="msg">
-            <h4>Registre seu pedido</h4>
-        </div>
-
-        <!--Formulário de pedidos-->
-        <form name="pedidos" class="form" action="" method="post">
-            <div class=form-label>
-                <label>Nome *</label>
-                <input name="name" type="text" required >
-            </div>
-
-            <div class="form-label">
-                <label>CPF *</label>
-                <input name="cpf" type="text" placeholder="000.000.000-00" cpf-mask="000.000.000-00" maxlength="14" required >
-            </div>
-
-            <div class="form-label">
-                <label>Endereço completo *</label>
-                <input name="address" type="text" placeholder="Ex.: Rua Exemplo, 94 ap 61 - Vl Anastacio" required >
-            </div>
-
-            <div class="form-label">
-                <label>CEP *</label>
-                <input name="cep" type="text" placeholder="00.000-000" cep-mask="00.000-000" maxlength="10" required >
-            </div>
-
-            <div class="form-label">
-                <label>Telefone *</label>
-                <input name="phone" type="number" required >
-            </div>
-
-            <div class="form-label">
-                <label>Categoria *</label>
-                <select name="category" required id="options">                
-                    <option value=""></option>
-                    <option value="geladeira">Geladeira</option>
-                    <option value="fogao">Fogão</option>
-                    <option value="microondas">Microondas</option>
-                    <option value="lavaroupa">Lavadora de roupas</option>
-                    <option value="lavalouca">Lava-louça</option>
-                </select>
-            </div>
-
-            <div class="form-label">
-                <label>Descrição *</label>
-                <input name="description" type="text" required >
-            </div>
-
-            <div class="form-label">
-                <label>Quantidade *</label>
-                <input name="quant" type="number" required >
-            </div>            
-
-            <div class="form-label obs">	
-                <label>Observações</label>
-                <textarea name="observ" type="text" maxlength="200"></textarea>
-            </div>
-
-            <div>
-                <button class="btn-enviar" type="submit" name="enviar">Enviar</button>
-            </div>
-        </form>
-
-        <!--Exibindo os pedidos-->
-        <section class="contact">
-            <?php
-                $sql_pedidos = "SELECT * FROM pedidos";
-                $result = $conn->query($sql_pedidos);
-
-                if($result->num_rows > 0) {
-                    while($row = $result->fetch_assoc()) {
-                        echo    "ID: ".$row['idpedido']."<br/>".
-                                "Data: ".$row['data']."<br/>".
-                                "Nome: ".$row['nome_cliente']."<br/>".
-                                "CPF: ".$row['cpf']."<br/>".
-                                "Endereço: ".$row['endereco']."<br/>".
-                                "CEP: ".$row['cep']."<br/>".
-                                "Telefone: ".$row['telefone']."<br/>".
-                                "Categoria: ".$row['categoria']."<br/>".
-                                "Produto: ".$row['descricao']."<br/>".
-                                "Quant.: ".$row['quantidade']."<br/>".
-                                "Observação: ".$row['observacao']."<br/><br/>";
+                <?php
                     }
                 } else {
-                    echo "Nenhum pedido ainda...";
-                }                        
+                    echo "Nenhum produto cadastrado. Tente outra vez.";
+                }        
+                ?>
+            </section>
+
+
+            <!---------FORM SECTION-------->
+            <section class="form center">
+                <h4 class="tittle">Registre seu pedido</h4>
+
+                <form name="pedidos" action="" method="post">
+                    <div class="form-row">
+                        <div class="form-group col-md form-style">
+                            <label>Nome</label>
+                            <input class="form-control" name="name" type="text" required>
+                        </div>                        
+                    </div>
+
+                    <div class="form-row">
+                        <div class="form-group col-md-6 form-style">                           
+                            <label>Endereço</label>
+                            <input class="form-control" name="address" type="text" placeholder="Ex.: Rua Exemplo, 94 ap 61 - Vl Anastacio" required>
+                        </div>
+
+                        <div class="form-group col-md-3 form-style">
+                            <label>CEP</label>
+                            <input class="form-control" name="cep" type="text" placeholder="00.000-000" cep-mask="00.000-000" maxlength="10" required>
+                        </div>
+
+                        <div class="form-group col-md-3 form-style">
+                            <label>Telefone</label>
+                            <input class="form-control" name="phone" type="number" placeholder="(11) 99999-9999" required>
+                        </div>
+                    </div>  
+                             
+                    <div class="form-row">
+                        <div class="form-group col-md-3 form-style">
+                            <label>Categoria</label>
+                            <select class="form-control" name="category" id="options">
+                                <option selected>Escolha..</option>
+                                <option value="geladeira">Geladeira</option>
+                                <option value="fogao">Fogão</option>
+                                <option value="microondas">Microondas</option>
+                                <option value="lavaroupa">Lavadora de roupas</option>
+                                <option value="lavalouca">Lava-louça</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-md-8 form-style">
+                            <label>Descrição</label>
+                            <input class="form-control" name="description" type="text" placeholder="Microondas Electrolux 20 Litros" required>
+                        </div>
+
+                        <div class="form-group col-md-1 form-style">
+                            <label>Quantidade</label>
+                            <input class="form-control" name="quant" type="number" required>
+                        </div> 
+                    </div>    
+
+                    <div class="form-row">
+                        <div class="form-group col-md form-style">
+                            <label for="exampleFormControlTextarea1">Observações</label>
+                            <textarea class="form-control" name="observ" type="text" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                    </div>
+
+                    <div>
+                        <button class="btn-standard" type="submit" name="enviar">Enviar</button>
+                    </div>
+                </form>  
+            </section>
+        
+
+            <!-----Exibição dos pedidos------>
+            <section class="show">
+                <?php
+                    $sql_pedidos = "SELECT * FROM pedidos";
+                    $result = $conn->query($sql_pedidos);
+
+                    if($result->num_rows > 0) {
+                        while($row = $result->fetch_assoc()) {
+                            echo    "ID: ".$row['idpedido']."<br/>".
+                                    "Data: ".$row['data']."<br/>".
+                                    "Nome: ".$row['nome_cliente']."<br/>".
+                                    "CPF: ".$row['cpf']."<br/>".
+                                    "Endereço: ".$row['endereco']."<br/>".
+                                    "CEP: ".$row['cep']."<br/>".
+                                    "Telefone: ".$row['telefone']."<br/>".
+                                    "Categoria: ".$row['categoria']."<br/>".
+                                    "Produto: ".$row['descricao']."<br/>".
+                                    "Quant.: ".$row['quantidade']."<br/>".
+                                    "Observação: ".$row['observacao']."<br/><br/>";
+                                }
+                    } else {
+                        echo "Nenhum pedido ainda...";
+                    }                        
+                ?>
+            </section>
+
+            <!----------FOOTER SECTION---------->
+            <?php
+                include('footer.html');
             ?>
-        </section>
+            
+        </div>  
 
-        <!--Rodape-->
-        <?php
-            include('footer.html');
-        ?>        
-
-        <script src="../scripts/main.js"></script>        
+        <script src="../scripts/main.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" 
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" 
+        integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>        
     </body>
 </html>
--- webpages/stores.php
+++ webpages/stores.php
@@ -1,59 +1,67 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="../styles/main.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <title>Lojas - Full Stack Eletro</title>
     </head>
 
     <body>
 
-        <!--Menu-->
+        <!----------MENU SECTION------------>
         <?php
             include('menu.html');
-        ?>    
-        <!--End Menu-->
-       
-        <header class="header">
-            <h1>Lojas</h1>        
-        </header>
-        <hr>        
+        ?>
         
-        <!--Lojas-->
-        <section class="store">
-            <div class="address">
-                <h4>Rio de Janeiro</h4>
-                <p>Avenida Presidente Vargas</p>
-                <p>n&ordm; 5000</p>
-                <p>10&ordm; andar</p>
-                <p>Centro</p>
-                <p>(21) 3333-3333</p>
+        <div class="container-fluid stores-grid">
+       
+            <!---------STORES SECTION--------> 
+            <div id="div1">
+                <img class="img-fluid img-thumbnail" src="../images/store.jpg" alt="loja">
             </div>
 
-            <div class="address">
-                <h4>São Paulo</h4>
+            <div class="card card-body text-center" id="div2">
+                <h4 class="card-header"><strong>São Paulo</strong></h4>
                 <p>Avenida Paulista</p>
-                <p>n&ordm; 985</p>
-                <p>3&ordm; andar</p>
-                <p>Jardins</p>
+                <p>n&ordm; 985 - 3&ordm; andar - Jardins</p>
                 <p>(11) 4444-4444</p>
             </div>
 
-            <div class="address">
-                <h4>Santa Catarina</h4>
+            <div class="card card-body text-center" id="div3">
+                <h4 class="card-header"><strong>Rio de Janeiro</strong></h4>
+                <p>Avenida Presidente Vargas</p>
+                <p>n&ordm; 5000 - 10&ordm; andar - Centro</p>
+                <p>(21) 3333-3333</p>
+            </div>
+
+            <div class="card card-body text-center" id="div4">
+                <h4 class="card-header"><strong>Santa Catarina</strong></h4>
                 <p>Rua Major I</p>
-                <p>Térreo</p>
-                <p>n&ordm; 370</p>                  
-                <p>Vila Mariana</p>
+                <p>Térreo - n&ordm; 370 - Vila Mariana</p>                  
                 <p>(47) 5555-5555</p>
             </div>
-        </section>    
 
-        <!--Rodape-->
-        <?php
-            include('footer.html');
-        ?>  
+            <div class="card card-body text-center" id="div5">
+                <h4 class="card-header"><strong>Belo Horizonte</strong></h4>
+                <p>Pç Sete de Setembro</p>
+                <p>n&ordm; 48 - 1&ordm; andar - Centro</p>
+                <p>(11) 6666-6666</p>
+            </div>   
+        </div>
 
+            <!----------FOOTER SECTION---------->
+            <?php
+                include('footer.html');
+            ?>        
+          
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" 
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" 
+        integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     </body>
 </html>
